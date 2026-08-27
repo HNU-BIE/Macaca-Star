afe20e1f5d2a8a8c81084bbe1b8df0abfea0db93
zzb <zauber@hainanu.edu.cn>
2026-08-27 21:27:02 +0800

    update

--- utils/PI_MRI.py
+++ utils/PI_MRI.py
@@ -6,9 +6,10 @@
 @Author  ：Zauber
 @Date    ：2024/6/3
 """
-from utils.CycleGan_3D.test import PI_to_T1_cyclegan
+from utils.CycleGan_3D.test import PI_to_T1_cyclegan, PI_to_T1_cyclegan_, GFP_to_T2_cyclegan
 from utils.fMOST_PI_preproc import tif_to_nii, normalize_to_8bit, denoise_img, remove_artifact, mas_cerebellum, \
-    clahe_image, intensity_c, PI_alignNMT, correct_T1like, fMOST_PI_3Dreg
+    clahe_image, intensity_c, PI_alignNMT, correct_T1like, fMOST_PI_3Dreg, upsample_toOrigin, mergePI, repair_atlas, \
+    seg_byt1pi
 
 
 def PI_preproc():
@@ -22,4 +23,8 @@ def PI_preproc():
     # PI_alignNMT()
     PI_to_T1_cyclegan()
     correct_T1like()
+    # mergePI()
     fMOST_PI_3Dreg()
+    # seg_byt1pi()
+    # repair_atlas()
+    # upsample_toOrigin()

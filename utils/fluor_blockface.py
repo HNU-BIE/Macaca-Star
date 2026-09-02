22950189ebe7a2f73b0e947872d05130095314f0
zzb <zauber@hainanu.edu.cn>
2026-09-02 11:37:32 +0800

    update

--- utils/fluor_blockface.py
+++ utils/fluor_blockface.py
@@ -1,4 +1,4 @@
-from utils.CycleGan_2D.test import fluor_toB_cyclegan
+from utils.CycWaveM2D.test import fluor_toB_cyclegan
 from utils.fluor_preproc import repaire_blikefluo, fluor_SyNtoB_bySeg, fluor_SyNtoB_bySections, \
     atlas_regtoAffinAFSlices, atlas_regtoRawFluoSlices, fluor_SyNtoB_byVolume, oc_fluor_toNMT
 
